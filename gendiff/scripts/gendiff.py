47c39023ea6aa3d00507a3864caff08d2d5f2eed
Aleksey Metelskiy <alnoroid@gmail.com>
2025-03-12 23:36:48 +0300

    added format as optional arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,6 +10,10 @@ parser._optionals.title = 'optional arguments'
 
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument(
+        '-f', '--format',
+        help='set format of output'
+    )
 
 args = parser.parse_args()
 
